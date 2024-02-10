9a20ffa09fa9407f7057ff9be905cef02514563f
拓海 宮崎 <lothric111@outlook.jp>
2024-02-10 20:43:50 +0900

    #1

--- noTitle.cpp
+++ noTitle.cpp
@@ -1,13 +1,81 @@
 ﻿// noTitle.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
 //
 
+//コード内容：シューティングゲーム的なもの
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
+//マップ：10行15列の横長マップ
+#define sizeH 10
+#define sizeW 20
+#define HP 3
+
+//マップの出力関数
+void output_map(char map[sizeH][sizeW]) {
+    for (int i = 0; i < sizeH; i++) {
+        for (int j = 0; j < sizeW; j++) {
+            printf("%c", map[i][j]);
+        }
+        putchar('\n');
+    }
+}
+
+//マップの初期化関数
+void map_reset(char map[sizeH][sizeW]) {
+    int temp = 0; //乱数の受け取り用
+
+    for (int i = 0; i < sizeH; i++) {
+        for (int j = 0; j < sizeW; j++) {
+            //自機の配置
+            if (i == 5 && j == 0) {
+                map[i][j] = 'A';
+            }
+
+            //敵機の配置
+            else if (j == 19) {
+                temp = rand() % 3; //０～２
+
+                //1/3の確率で敵を配置
+                if (temp == 0) {
+                    map[i][j] = 'e'; //小型敵機
+                }
+                else {
+                    map[i][j] = '~'; //空白部分
+                }
+            }
+
+            //空白部分の配置
+            else {
+                map[i][j] = '~';
+            }
+        }
+    }
+}
+
+//ゲーム開始時に初期化が必要なものは関数内、それ以外はmain内に記述
 int main()
 {
-    
+    //乱数生成用
+    srand((unsigned int)time(NULL));
+
+    //マップの作成
+    char map[sizeH][sizeW]; //ゲームマップ
+
+    //ゲーム開始
+    int i = 0;
+
+    while (i == 0) {
+        //マップの初期化
+        map_reset(map);
+        output_map(map);
+        i++;
+
+        //
+    }
+
+    return 0;
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
